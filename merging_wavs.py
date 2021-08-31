f876e01abe574cc8536e4ce69a75e1ab5be89c2a
Olaf Kranse <54064523+OlafKranse@users.noreply.github.com>
2021-08-31 02:10:03 +0100

    Update merging_wavs.py

--- merging_wavs.py
+++ merging_wavs.py
@@ -10,19 +10,19 @@ list_of_dirs = os.listdir(path_to_dir)
 for dirs in list_of_dirs:
     the_numbers_list =  []
     list_in_files = []
-    list_of_files = os.listdir(path_to_dir+"/"+dirs)
+    list_of_files = os.listdir(path_to_dir)
     for wavs in list_of_files:
         the_numbers = wavs.split(".wav")[0]
         the_numbers_list.append(int(the_numbers))
     the_numbers_list.sort(key=int)
     for things in the_numbers_list:
-        list_in_files.append("out"+"/"+dirs+"/"+str(things)+".wav")  ###test needs to be changed later into the real dir
+        list_in_files.append("out"+"/"+str(things)+".wav")  ###test needs to be changed later into the real dir
     infiles = list_in_files
     for files in infiles:
         #print(files)
         data, samplerate = soundfile.read(files)
         soundfile.write(files, data, samplerate, subtype="PCM_16")
-    outfile = ("out"+"/"+dirs+".wav")###test needs to be changed later into the real dir
+    outfile = ('out'+".wav")###test needs to be changed later into the real dir
     data= []
     for infile in infiles:
         w = wave.open(infile, 'rb')
